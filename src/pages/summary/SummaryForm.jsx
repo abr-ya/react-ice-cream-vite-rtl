c8ee0010f6cd1aa149786ba70628e21cb2ea95c1
abr-ya <gamer05@list.ru>
2021-08-19 18:22:24 +0300

    test: Add SummaryForm test

---
+++ src/pages/summary/SummaryForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SummaryForm = () => {
+  // start component
+  console.log('temp');
+
+  return (
+    <div>Summary form</div>
+  );
+};
+
+export default SummaryForm;
---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SummaryForm from '../SummaryForm';
+
+test('Initial conditions', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test('Checkbox enables button on first click and disables on second click', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
