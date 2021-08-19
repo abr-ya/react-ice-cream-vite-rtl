cc6ac2cd8a1a814b93308c42ae98f8ae04117b9e
abr-ya <gamer05@list.ru>
2021-08-19 19:57:41 +0300

    feat: Checkbox and button on Summary Page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,50 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
+import SummaryForm from '../../pages/summary/SummaryForm';
 import './app.scss';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <h1 className="app-header">Hello from Vite + React!</h1>
-      <p>
-        <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
-          Add 1 to counter
-        </button>
-      </p>
-      <p>
-        Counter value is:
-        {' '}
-        {count}
-      </p>
-      <p>
-        Edit
-        {' '}
-        <code>App.jsx</code>
-        {' '}
-        and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {' | '}
-        <a
-          className="app-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </div>
-  );
-};
+const App = () => (
+  <SummaryForm />
+);
 
 export default App;
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,19 +1,19 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-undef */
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react'; // screen
 import App from './App';
 
 // eslint-disable-next-line no-undef
 it('renders Header and it has correct class', () => {
   render(<App />);
-  const Header = screen.getByText('Hello from Vite + React!');
-  expect(Header).toBeEnabled();
-  expect(Header).toHaveClass('app-header');
+  // const Header = screen.getByText('Hello from Vite + React!');
+  // expect(Header).toBeEnabled();
+  // expect(Header).toHaveClass('app-header');
 });
 
-it('renders Add counter button', () => {
-  render(<App />);
-  const addButton = screen.getByRole('button', { name: 'Add 1 to counter' });
-  expect(addButton).toBeInTheDocument();
-});
+// it('renders Add counter button', () => {
+//   render(<App />);
+//   const addButton = screen.getByRole('button', { name: 'Add 1 to counter' });
+//   expect(addButton).toBeInTheDocument();
+// });
